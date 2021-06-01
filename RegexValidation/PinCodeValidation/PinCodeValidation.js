8e466ad5a2be359aa8d8015517a0f09556b64c66
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-01 06:32:25 +0530

    Added:To validate PinCode with 6 digits and restrict alphabets at beginning

--- RegexValidation/PinCodeValidation/PinCodeValidation.js
+++ RegexValidation/PinCodeValidation/PinCodeValidation.js
@@ -1,7 +1,7 @@
-
-function validatePincode(pincode) {
-   var regex = RegExp('^[1-9]{1}[0-9]{5}$');
-   if(regex.test(pincode)){
+//UC-1 To Validate 6 digit Pincode
+function validatePincodewithpattern1(pincode) {
+   var pattern1 = RegExp('^[1-9]{1}[0-9]{5}$');
+   if(pattern1.test(pincode)){
       console.log("Valid PinCode!");
       return true;
    }
@@ -11,5 +11,21 @@ function validatePincode(pincode) {
    }
 }
 
-let pincode = '400088';
-validatePincode(pincode); 
+let pincode1 = '400088';
+validatePincodewithpattern1(pincode1);
+
+//UC-2 To restict alphabets at the beginning
+function validatePincodewithpattern2(pincode) {
+   var pattern2 = RegExp('^[a-z A-Z!@#$%^&*]{1}[0-9]{5}$');
+   if(pattern2.test(pincode)){
+      console.log("Valid PinCode!");
+      return true;
+   }
+   else {
+      console.log("Invalid PinCode!");
+      return false;
+   }
+} 
+
+let pincode2 = 'A400088';
+validatePincodewithpattern2(pincode2);
